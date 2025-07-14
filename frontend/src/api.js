530f206be51537424ef519e3d5208e8e434d2c32
Ruhan-Saad-Dave <149294639+Ruhan-Saad-Dave@users.noreply.github.com>
2025-07-14 21:30:40 +0530

    chore: Web page unable to create a story, so made the request fix in the frontend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "/api";
+const API_URL = `${import.meta.env.VITE_API_URL || ''}/api`;
 
 export const apiClient = axios.create({
     baseURL: API_URL,
